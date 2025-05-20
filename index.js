33243e15b080f3cd4996cf776985a99eb8903b34
Lucas <lucas.batista.flooxai@gmail.com>
2025-05-20 13:34:27 -0300

    feat: variables javascript, operators, else if, switch case, for

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
-/**
- * Comentários
- */
+// Variaveis
 
-console.log("Olá Mundo!")
-/*console.log("Olá Mundo!")
-console.log("Olá Mundo!")*/
+// var olaMundo = "Olá Mundo!!!!!!!!!!"
+
+// console.log(olaMundo)
+// console.log(olaMundo)
+// console.log(olaMundo)
+// console.log(olaMundo)
+
+// Operadores
+
+// let a = 10
+// const b = "20"
+
+// console.log(a != b || typeof a == 'string')
+
+// var cor = "amarelo";
+
+// if (cor === "verde") {
+//   console.log("Siga!");
+// } else if (cor === "amarelo") {
+//   console.log("Atenção");
+// } else if (cor === "vermelho"){
+//     console.log("vermelho")
+// } else {
+//   console.log("Não siga!");
+// }
+
+// let cor = "azul";
+
+// switch (cor) {
+//   case "verde":
+//     console.log("Siga");
+//     break;
+
+//   case "amarelo":
+//     console.log("Atenção");
+//     break;
+
+//   case "vermelho":
+//     console.log("Pare");
+//     break;
+
+//   default:
+//     console.log("Não sei");
+// }
+
+// var n = 5
+
+// for(let i = 0; i<=10; i++) {
+
+//     console.log(`${i} X ${n} = ${i * n}`)
+
+// }
